dc067255d79d0b045d61604979d2015ff2637c49
Alexey Belkin <alexey.belkin@mail.ru>
2023-09-30 02:04:33 +0800

    formatting change

--- src/App.js
+++ src/App.js
@@ -1,21 +1,21 @@
-import {BingSat} from './layers/bingsat'
-export {BingSat}
-import {BasicOsm} from './layers/basicosm'
-export {BasicOsm}
-import {OpenTopoMap, OpenTopoMapCZ} from './layers/opentopomap'
-export {OpenTopoMap, OpenTopoMapCZ}
-import {YandexSatellite, YandexMaps, YandexTracks, YandexHybrid} from './layers/yandex'
-export {YandexSatellite, YandexMaps, YandexTracks, YandexHybrid}
-import {Topomapper} from './layers/topomapper'
-export {Topomapper}
-import {GosGisCenter} from './layers/ggc'
-export {GosGisCenter}
+import { BingSat } from './layers/bingsat'
+export { BingSat }
+import { BasicOsm } from './layers/basicosm'
+export { BasicOsm }
+import { OpenTopoMap, OpenTopoMapCZ } from './layers/opentopomap'
+export { OpenTopoMap, OpenTopoMapCZ }
+import { YandexSatellite, YandexMaps, YandexTracks, YandexHybrid } from './layers/yandex'
+export { YandexSatellite, YandexMaps, YandexTracks, YandexHybrid }
+import { Topomapper } from './layers/topomapper'
+export { Topomapper }
+import { GosGisCenter } from './layers/ggc'
+export { GosGisCenter }
 import { EsriSatellite } from './layers/esri'
 export { EsriSatellite } 
 import { PkkRosreestr } from './layers/rosreestr'
-export {PkkRosreestr}
+export { PkkRosreestr }
 import { GoogleSatellite } from './layers/google'
-export { GoogleSatellite}
+export { GoogleSatellite }
 import { MegafonCoverage, MtsRusCoverage, MtsByCoverage, A1ByCoverage, LifeByCoverage, Tele2Coverage, BeelineCoverage } from './layers/operators'
 export { MegafonCoverage, MtsRusCoverage, MtsByCoverage, A1ByCoverage, LifeByCoverage, Tele2Coverage, BeelineCoverage } 
 import { Strava } from './layers/strava'
