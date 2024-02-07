98ea2577a0a78a4dfee7594d759332a929b48b67
Alexey Belkin <alexey.belkin@mail.ru>
2024-02-08 00:30:33 +0800

    remove no longer working opensnowmap basemap

--- src/layers/snowmap.js
+++ src/layers/snowmap.js
@@ -4,17 +4,8 @@ import XYZ from 'ol/source/XYZ';
 export const OpenSnowMap = {
   id: 'oSN',
   title: 'Opensnowmap',
-  type: 'base',
-  tint: 'light-topo',
   visible: false,
   layers: [
-    new TileLayer({
-      preload: Infinity,
-      source: new XYZ({
-        maxZoom: 18,
-        url: 'https://tiles.opensnowmap.org/base_snow_map/{z}/{x}/{y}.png?debug1'
-      }),
-    }),
     new TileLayer({
       preload: Infinity,
       source: new XYZ({
