ca30ff06684196ecfac36fcd71eb8131f1b840c3
Alexey Belkin <alexey.belkin@mail.ru>
2024-10-19 23:24:41 +0800

    map as forward ref element created during DOM creation

--- src/components/mapcontainer.jsx
+++ src/components/mapcontainer.jsx
@@ -1,8 +1,9 @@
+/* eslint-disable solid/reactivity */
 import Map from 'ol/Map.js';
 import {Collection} from 'ol';
 import '../index.css';
 import '../../node_modules/ol/ol.css';
-import { createSignal, onMount } from 'solid-js';
+import { createSignal } from 'solid-js';
 import { useService } from 'solid-services';
 import {GPX} from 'ol/format.js';
 import { MapContext } from '../services/mapcontext';
@@ -13,6 +14,50 @@ import { PLT } from '../format/PLT';
 import DragAndDrop from 'ol/interaction/DragAndDrop';
 import { GisLink } from '../interactions/link';
 
+function MapInternal(props) {
+  const layerService = useService(LayerService);
+
+  layerService().basemaps = props.basemaps;
+  layerService().overlays = props.overlays;
+        
+  const interactions = props.interactions || defaults();
+
+  const options = {...{
+    controls: props.controls || new Collection(),
+    interactions: interactions,
+    layers: layerService().flat,
+    target: props.target(),
+    view: props.view,
+    maxTilesLoading: 64,
+    moveTolerance: 1 //default value
+  }, ...props.options};
+    
+  const indexMap = new Map(options);
+
+  let dragAndDropInteraction;
+
+  function setDragDropInteraction() {
+    if (dragAndDropInteraction) {
+      indexMap.removeInteraction(dragAndDropInteraction);
+    }
+    dragAndDropInteraction = new DragAndDrop({
+      formatConstructors: [
+        GPX,
+        ArrayGeoJSON,
+        PLT
+      ],
+    });
+    dragAndDropInteraction.on('addfeatures', (event)=> layerService().addFeaturesFromEvent(event, indexMap));
+    indexMap.addInteraction(dragAndDropInteraction);
+  }
+  setDragDropInteraction();
+  indexMap.addInteraction(new GisLink());
+
+  const getMap = useService(MapContext);
+  getMap().map = indexMap;
+}
+
+
 export const MapContainer = props => {
   // eslint-disable-next-line solid/reactivity
   if(!props.id) throw new Error('no map div id in props');
@@ -20,50 +65,10 @@ export const MapContainer = props => {
   if(!props.view) throw new Error('no View in props');
 
   let [mapDiv, setMapDiv] = createSignal(null);
-  onMount(() => {
-    const layerService = useService(LayerService);
-
-    layerService().basemaps = props.basemaps;
-    layerService().overlays = props.overlays;
-        
-    const interactions = props.interactions || defaults();
-
-    const options = {...{
-      controls: props.controls || new Collection(),
-      interactions: interactions,
-      layers: layerService().flat,
-      target: mapDiv(),
-      view: props.view,
-      maxTilesLoading: 64,
-      moveTolerance: 1 //default value
-    }, ...props.options};
-    
-    const indexMap = new Map(options);
-
-    let dragAndDropInteraction;
-
-    function setDragDropInteraction() {
-      if (dragAndDropInteraction) {
-        indexMap.removeInteraction(dragAndDropInteraction);
-      }
-      dragAndDropInteraction = new DragAndDrop({
-        formatConstructors: [
-          GPX,
-          ArrayGeoJSON,
-          PLT
-        ],
-      });
-      dragAndDropInteraction.on('addfeatures', (event)=> layerService().addFeaturesFromEvent(event, indexMap));
-      indexMap.addInteraction(dragAndDropInteraction);
-    }
-    setDragDropInteraction();
-    indexMap.addInteraction(new GisLink());
-
-    const getMap = useService(MapContext);
-    getMap().map = indexMap;
-  });
+ 
 
   return (<div id={props.id} ref={(el) => setMapDiv(el) } class='map-default'>
+    <MapInternal target={mapDiv} {...props}/>
     {props.children}
   </div>);
 };
